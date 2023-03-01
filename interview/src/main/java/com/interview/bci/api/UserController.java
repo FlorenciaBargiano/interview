2bb46e621ecaa4b07008d044d12085c4262895da
florencia.bargiano <ext-florencia.bargiano@here.com>
2023-03-01 11:21:05 -0300

    remove logic on the controller

--- interview/src/main/java/com/interview/bci/api/UserController.java
+++ interview/src/main/java/com/interview/bci/api/UserController.java
@@ -1,13 +1,11 @@
 package com.interview.bci.api;
 
-import com.interview.bci.entity.BadRequestException;
 import com.interview.bci.entity.User;
 import com.interview.bci.entity.UserResponse;
 import com.interview.bci.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,11 +23,7 @@ public class UserController {
     @PostMapping(value = "/sign-up",
             produces = "application/json",
             consumes = "application/json")
-    ResponseEntity<UserResponse> signUp(@Valid @RequestBody User user,
-                                        Errors errors) {
-        if (errors.hasErrors())
-            throw new BadRequestException(LocalDateTime.now(), errors.getAllErrors().get(0).getDefaultMessage());
-
+    ResponseEntity<UserResponse> signUp(@Valid @RequestBody User user) {
         return new ResponseEntity<>(userService.signUp(user), HttpStatus.CREATED);
     }
 
